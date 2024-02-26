6c92a9d5bc8591047c51e83e7d72d24e5e3e43a7
mercballs <34362886+mercballs@users.noreply.github.com>
2024-02-26 10:10:52 -0500

    bugfixes and added logging to logfile

--- bot.py
+++ bot.py
@@ -12,12 +12,14 @@ TODO
 - Get clock info from the arena timer.
 
 KNOWN ISSUES
-- This script is designed for only one match to be active at a time. There are issues with multiple matches being active at once.
+- This script is designed for only one match to be active at a time.
+  There are issues with multiple matches being active at once.
 '''
 
 import requests
 import json
 import time
+import datetime
 # import serial
 
 # true finals API info
@@ -30,11 +32,14 @@ headers = {
 botName = "EventBot"
 
 # discord webhook URL
-url = "https://discord.com/api/webhooks/ENTERHERE"
+url = "https://discord.com/api/webhooks/ENTER HERE"
 
 # ids of your truefinals brackets (will be found inside the actual page url
 # EX. https://truefinals.com/tournament/12345678abcdefgh
-ids = ["bf2a4479166448fc", "555be80d228b4daf"]
+ids = ["ENTER HERE", "ENTER HERE"]
+
+logFilename = "log.txt"
+logging = True;
 
 def getPlayer(playerId, playerData):
     for q in playerData:
@@ -48,14 +53,25 @@ def getWinner(matchId, matchData):
             return w["playerID"]
 
 
+def logIt(logstring):
+    if logging:
+        logfile = open(logFilename, "a")
+        logfile.write(str(datetime.datetime.now()) + " " + logstring + "\n")
+        logfile.close()
+        print(str(datetime.datetime.now()) + " " + logstring)
+
 states = {}
 oldStates = {}
+finished = {
+    ids[0]: 0,
+    ids[1]: 0
+}
 
 for ID in ids:
     i = 0
     req = requests.get("https://truefinals.com/api/v1/tournaments/" + ID, headers=headers)
     tourney = json.loads(req.text)
-    # print(tourney)
+    #print(tourney)
     req.close()
     games = tourney['games']
 
@@ -67,18 +83,18 @@ for ID in ids:
         time.sleep(1)
 
 jsonData = {
-  "p1name": "Player1 (0-0)",
-  "p1icon": "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
-  "p2name": "Player2 (0-0)",
-  "p2icon": "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
-  "timer": "",
-  "winner": "Player1",
-  "scroller": "",
-  "showPlayers": False,
-  "showWinner": False,
-  "showScroller": True,
-  "p1shortName": "Player1",
-  "p2shortName": "Player2",
+    "p1name": "Player1 (0-0)",
+    "p1icon": "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
+    "p2name": "Player2 (0-0)",
+    "p2icon": "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
+    "timer": "",
+    "winner": "Player1",
+    "scroller": "",
+    "showPlayers": False,
+    "showWinner": False,
+    "showScroller": True,
+    "p1shortName": "Player1",
+    "p2shortName": "Player2"
 }
 
 stopShow = 0
@@ -87,7 +103,9 @@ scroller = ["", "", ""]
 
 # ser = serial.Serial('COM5', 115200, timeout=1)
 
-while True: #forever loop, until match is over...
+logIt("Script Started")
+
+while True:  # forever loop, until match is over...
     print("loop")
 
     try:
@@ -97,20 +115,17 @@ while True: #forever loop, until match is over...
 
         for ID in ids:
             req = requests.get("https://truefinals.com/api/v1/tournaments/" + ID, headers=headers)
-            tourney = {}
             tourney = json.loads(req.text)
             # print(tourney)
             req.close()
-
-            players = {}
             players = tourney['players']
             games = tourney['games']
-
             oldStates[ID].clear()
             i = 0
             data = {
                 "username": botName
             }
+
             while i < len(games):
                 oldStates[ID].append("d")
                 oldStates[ID][i] = games[i]["state"]
@@ -130,12 +145,24 @@ while True: #forever loop, until match is over...
                                     "color": 0xF4B400,
                                     "url": "https://truefinals.com/api/v1/tournaments/" + ID,
                                     "fields": [
-                                        {'name': getPlayer(p1Id, players)['name'] + " (" + str(getPlayer(p1Id, players)['wins']) + "-" + str(getPlayer(p1Id, players)['losses']) + ") vs "
-                                                 + getPlayer(p2Id, players)['name'] + " (" + str(getPlayer(p2Id, players)['wins']) + "-" + str(getPlayer(p2Id, players)['losses']) + ")", "value": ""}
+                                        {
+                                            'name': getPlayer(p1Id, players)['name'] + " (" +
+                                                    str(getPlayer(p1Id, players)['wins']) + "-" +
+                                                    str(getPlayer(p1Id, players)['losses']) + ") vs " +
+                                                    getPlayer(p2Id, players)['name'] + " (" +
+                                                    str(getPlayer(p2Id, players)['wins']) + "-" +
+                                                    str(getPlayer(p2Id, players)['losses']) + ")",
+                                            "value": ""
+                                        }
                                     ]
                                 }
                             ]
                             result = requests.post(url, json=data)
+                            logIt(
+                                "Match Called (" + tourney['title'] + " - " + games[i]['name'] + "): " +
+                                getPlayer(p1Id, players)['name'] + " vs " + getPlayer(p2Id, players)['name']
+                            )
+
                         case "active":
                             data["embeds"] = [
                                 {
@@ -145,23 +172,39 @@ while True: #forever loop, until match is over...
                                     "color": 0xff0000,
                                     "url": "https://truefinals.com/api/v1/tournaments/" + ID,
                                     "fields": [
-                                        {'name': getPlayer(p1Id, players)['name'] + " (" + str(getPlayer(p1Id, players)['wins']) + "-" + str(getPlayer(p1Id, players)['losses']) + ") vs "
-                                                 + getPlayer(p2Id, players)['name'] + " (" + str(getPlayer(p2Id, players)['wins']) + "-" + str(getPlayer(p2Id, players)['losses']) + ")", "value": ""}
+                                        {
+                                            'name': getPlayer(p1Id, players)['name'] + " (" +
+                                                    str(getPlayer(p1Id, players)['wins']) + "-" +
+                                                    str(getPlayer(p1Id, players)['losses']) + ") vs " +
+                                                    getPlayer(p2Id, players)['name'] + " (" +
+                                                    str(getPlayer(p2Id, players)['wins']) + "-" +
+                                                    str(getPlayer(p2Id, players)['losses']) + ")",
+                                            "value": ""
+                                        }
                                     ]
                                 }
                             ]
                             result = requests.post(url, json=data)
+                            logIt(
+                                "Match Active (" + tourney['title'] + " - " + games[i]['name'] + "): " +
+                                getPlayer(p1Id, players)['name'] + " vs " + getPlayer(p2Id, players)['name']
+                            )
                             jsonData["showPlayers"] = True
                             jsonData["showWinner"] = False
-                            jsonData['p1name'] = getPlayer(p1Id, players)['name'] + " (" + str(getPlayer(p1Id, players)['wins']) + "-" + str(getPlayer(p1Id, players)['losses']) + ")"
+                            jsonData['p1name'] = (getPlayer(p1Id, players)['name'] + " (" +
+                                                  str(getPlayer(p1Id, players)['wins']) + "-" + str(
+                                                  getPlayer(p1Id, players)['losses']) + ")")
                             jsonData["p1icon"] = getPlayer(p1Id, players)['photoUrl']
                             jsonData['p1shortName'] = getPlayer(p1Id, players)['name']
-                            jsonData["p2name"] = getPlayer(p2Id, players)['name'] + " (" + str(getPlayer(p2Id, players)['wins']) + "-" + str(getPlayer(p2Id, players)['losses']) + ")"
+                            jsonData["p2name"] = (getPlayer(p2Id, players)['name'] + " (" +
+                                                  str(getPlayer(p2Id, players)['wins']) + "-" + str(
+                                                  getPlayer(p2Id, players)['losses']) + ")")
                             jsonData["p2icon"] = getPlayer(p2Id, players)['photoUrl']
                             jsonData["p2shortName"] = getPlayer(p2Id, players)['name']
                             stopShow = 0
+
                         case "done":
-                            #print(games)
+                            # print(games)
                             wid = getWinner(i, games)
                             data["embeds"] = [
                                 {
@@ -171,80 +214,101 @@ while True: #forever loop, until match is over...
                                     "color": 0x1ba300,
                                     "url": "https://truefinals.com/api/v1/tournaments/" + ID,
                                     "fields": [
-                                        {'name': getPlayer(p1Id, players)['name'] + " (" + str(getPlayer(p1Id, players)['wins']) + "-" + str(getPlayer(p1Id, players)['losses']) + ") vs "
-                                                 + getPlayer(p2Id, players)['name'] + " (" + str(getPlayer(p2Id, players)['wins']) + "-" + str(getPlayer(p2Id, players)['losses']) + ")", "value": ""},
+                                        {'name': getPlayer(p1Id, players)['name'] + " (" +
+                                                 str(getPlayer(p1Id, players)['wins']) + "-" +
+                                                 str(getPlayer(p1Id, players)['losses']) + ") vs " +
+                                                 getPlayer(p2Id, players)['name'] + " (" +
+                                                 str(getPlayer(p2Id, players)['wins']) + "-" +
+                                                 str(getPlayer(p2Id, players)['losses']) + ")", "value": ""},
                                         {'name': getPlayer(wid, players)['name'] + " WINS!", "value": ""}
                                     ]
                                 }
                             ]
                             result = requests.post(url, json=data)
-                            if jsonData["p1shortName"] == getPlayer(p1Id, players)['name'] and jsonData["p2shortName"] == getPlayer(p2Id, players)['name']:
+                            logIt(
+                                "Match Done (" + tourney['title'] + " - " + games[i]['name'] + "): " +
+                                getPlayer(p1Id, players)['name'] + " vs " + getPlayer(p2Id, players)['name'] + ", " +
+                                getPlayer(wid, players)['name'] + " Wins"
+                            )
+                            if jsonData["p1shortName"] == getPlayer(p1Id, players)['name'] and jsonData[
+                                "p2shortName"] == getPlayer(p2Id, players)['name']:
                                 jsonData["showWinner"] = True
                                 jsonData["winner"] = getPlayer(wid, players)['name']
                                 stopShow = 5
                             scroller.pop(0)
-                            scroller.append(getPlayer(p1Id, players)['name'] + " vs " + getPlayer(p2Id, players)['name'] + " = " + getPlayer(wid, players)['name'] + " WINS!")
-                            jsonData["scroller"] = "Recent Matches: " + scroller[0] + " | " + scroller[1] + " | " + scroller[2]
+                            scroller.append(
+                                getPlayer(p1Id, players)['name'] + " vs " + getPlayer(p2Id, players)['name'] + " = " +
+                                getPlayer(wid, players)['name'] + " WINS!")
+                            jsonData["scroller"] = "Recent Matches: " + scroller[0] + " | " + scroller[1] + " | " + \
+                                                   scroller[2]
+
                         case _:
                             pass
                     states[ID][i] = oldStates[ID][i]
                 i += 1
 
-        #print(stopShow)
-        if stopShow == 1:
-            jsonData["showPlayers"] = False
-            jsonData["showWinner"] = False
-            stopShow -= 1
-        elif stopShow > 0:
-            stopShow -= 1
+            # print(stopShow)
+            if stopShow == 1:
+                jsonData["showPlayers"] = False
+                jsonData["showWinner"] = False
+                stopShow -= 1
+            elif stopShow > 0:
+                stopShow -= 1
 
-        f = open("data.json", "w")
-        f.write(json.dumps(jsonData))
-        f.close()
+            f = open("data.json", "w")
+            f.write(json.dumps(jsonData))
+            f.close()
 
-        winners = [0,0,0]
-        if not tourney['endTime'] is None: #tourney is complete
-            print("tournament complete!")
-            print(players)
-            for pl in players:
-                place = getPlayer(pl['id'], players)['placement']
-                if place is not None and place == 1:
-                    winners[0] = pl['id']
-                if place is not None and place == 2:
-                    winners[1] = pl['id']
-                if place is not None and place == 3:
-                    winners[2] = pl['id']
+            winners = [0, 0, 0]
+            # print(tourney['title'] + ": " + str(tourney['endTime']))
+            if (not tourney['endTime'] is None) and (finished[ID] == 0):  # tourney is complete
+                finished[ID] = 1
+                print("tournament complete!")
+                # print(players)
+                for pl in players:
+                    place = getPlayer(pl['id'], players)['placement']
+                    if place is not None and place == 1:
+                        winners[0] = pl['id']
+                    if place is not None and place == 2:
+                        winners[1] = pl['id']
+                    if place is not None and place == 3:
+                        winners[2] = pl['id']
 
-            data["embeds"] = [
-                {
-                    "type": "rich",
-                    "title": tourney['title'] + " Tournament Complete!",
-                    "description": "",
-                    "color": 0xffc0cb,
-                    "url": "https://truefinals.com/api/v1/tournaments/" + ID,
-                    "fields": [
-                        {
-                            'name': "First Place:",
-                            'value': getPlayer(winners[0], players)['name'] + " (" + str(getPlayer(winners[0], players)['wins']) + "-" + str(getPlayer(winners[0], players)['losses']) + ") "
-                        },
-                        {
-                            'name': "Second Place:",
-                            'value': getPlayer(winners[1], players)['name'] + " (" + str(
-                                getPlayer(winners[1], players)['wins']) + "-" + str(
-                                getPlayer(winners[1], players)['losses']) + ") "
-                        },
-                        {
-                            'name': "Third Place:",
-                            'value': getPlayer(winners[2], players)['name'] + " (" + str(
-                                getPlayer(winners[2], players)['wins']) + "-" + str(
-                                getPlayer(winners[2], players)['losses']) + ") "
-                        },
-                    ]
-                }
-            ]
-            result = requests.post(url, json=data)
-            #print("exiting...")
-            #break
+                data["embeds"] = [
+                    {
+                        "type": "rich",
+                        "title": tourney['title'] + " Tournament Complete!",
+                        "description": "",
+                        "color": 0xffc0cb,
+                        "url": "https://truefinals.com/api/v1/tournaments/" + ID,
+                        "fields": [
+                            {
+                                'name': "First Place:",
+                                'value': getPlayer(winners[0], players)['name'] + " (" +
+                                         str(getPlayer(winners[0], players)['wins']) + "-" +
+                                         str(getPlayer(winners[0], players)['losses']) + ") "
+                            },
+                            {
+                                'name': "Second Place:",
+                                'value': getPlayer(winners[1], players)['name'] + " (" +
+                                         str(getPlayer(winners[1], players)['wins']) + "-" +
+                                         str(getPlayer(winners[1], players)['losses']) + ") "
+                            },
+                            {
+                                'name': "Third Place:",
+                                'value': getPlayer(winners[2], players)['name'] + " (" +
+                                         str(getPlayer(winners[2], players)['wins']) + "-" +
+                                         str(getPlayer(winners[2], players)['losses']) + ") "
+                            },
+                        ]
+                    }
+                ]
+                result = requests.post(url, json=data)
+                logIt(
+                    "Tourney Done (" + tourney['title'] + "): 1st: " + getPlayer(winners[0], players)['name'] +
+                    ", 2nd: " + getPlayer(winners[1], players)['name'] +
+                    ", 3rd: " + getPlayer(winners[2], players)['name']
+                )
 
     except requests.exceptions.RequestException as e:  # This is the correct syntax
         print('http request error: ')
